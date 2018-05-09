eb44ecc0393aba0e8c425842933b0df1219f500a
JhanaeA <37199132+JhanaeA@users.noreply.github.com>
2018-05-08 21:52:34 -0400

    Add files via upload

---
+++ chakraIn.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  chakraInformation
+//
+//  Created by Jhanae Askew on 5/8/18.
+//  Copyright © 2018 Jhanae Askew. All rights reserved.
+//
+
+import Foundation
+
+class chakraIn{
+    
+    public var rootLocation = "Coccyx/Perineum"
+    public var sacralLocation = "Sacrum/Pelvis"
+    public var solarPlexusLocation = "Upper Abdomen"
+    public var heartLocation = "Chest"
+    public var throatLocation = "Adams Apple"
+    public var browLocation = "Forehead"
+    public var crownLocation = "Slightly above the head"
+    
+    public var rootCharacteristics : [String] = ["Grounded", "Physical health", "Comfort in own body", "Sense of safety and security", "Stability and security", "Right livelihood", "Prosperity", "Ability to be still", "Presence in the here and now"]
+    public var sacralCharacteristics : [String] = ["Graceful Movements", "Ability to embrace change", "Emotioanal intelligence", "Nurturance of self and others", "Healthy boundaries", "Ability to enjoy pleasure", "Passion"]
+    public var solarPlexusCharacteristics : [String] = ["Responsible", "Reliable", "Good self-discipline", "Positive sense of self", "Confident", "Warm", "Energetic", "Spontaneous", "Playful", "Humorous", "Ability to take risks"]
+    public var heartCharacteristics : [String] = ["Caring", "Compassionate", "Empathetic", "Accepting", "Self-loving", "Peaceful", "Centered", "Content"]
+    public var throatCharacteristics : [String] = ["Resonant", "Full voice", "Clear communication with others", "Good communication with self", "Good listener", "Good sense of timing and rhythm", "Live life creatively"]
+    public var browCharacteristics : [String] = ["Strong intuition", "Penetrating insight", "Creative imagination", "Good memory", "Good dream recall", "Ability to visualize", "Have a guiding vision for life"]
+    public var crownCharacteristics : [String] = ["Spiritual connection", "Wisdom and mastery", "Intelligence", "Presence", "Open-mindedness", "Ability to question", "Ability to assimilate and analyzee information"]
+
+}
+
